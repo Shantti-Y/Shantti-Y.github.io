82484f5d50d94b0681bd366f393c06059661e0b1
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-07-26 00:14:36 +0900

    Add Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,38 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+const entryDir = '../';
+
+module.exports = ({ config }) => {
+
+  config.resolve.extensions.push('.ts', '.tsx', '.vue', '.css', '.less', '.scss', '.sass', '.html');
+
+  config.module.rules.push({
+    test: /\.(ts)$/,
+    exclude: /node_modules/,
+    loader: 'ts-loader',
+    options: {
+      appendTsSuffixTo: [/\.vue$/],
+      transpileOnly: true
+    },
+  });
+
+  config.module.rules.push({
+    test: /\.vue$/,
+    exclude: /node_modules/,
+    loader: 'vue-loader'
+  });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"]
+  });
+
+  config.resolve.alias = {
+    '@/components': path.resolve(__dirname, `${entryDir}/components`),
+    'vue$': 'vue/dist/vue.esm.js'
+  }
+
+  config.plugins.push(new ForkTsCheckerWebpackPlugin())
+
+  return config;
+};
