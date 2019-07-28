b0d66f4a617d168f667b38f2482adad12f50d841
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-07-28 15:21:08 +0900

    refine layout design on desktop

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="default">
-    <header-nav />
-    <nuxt />
-    <footer-nav />
+  <div id="default" :style="backgroundStyle">
+    <desktop-nav-menu />
+    <nuxt id="page-contents" ref="page" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Getter } from 'vuex-class';
 
-import HeaderNav from '@/components/organisms/Header.vue';
-import FooterNav from '@/components/organisms/Footer.vue';
+import DesktopNavMenu from '@/components/organisms/DesktopNavMenu.vue';
+import MobileNavMenu from '@/components/organisms/MobileNavMenu.vue';
 
 @Component({
   components: {
-    HeaderNav,
-    FooterNav
+    DesktopNavMenu,
+    MobileNavMenu
   }
 })
 export default class DefaultLayout extends Vue {
+  @Getter('background/currentImage') readonly currentImage;
+
+  get backgroundStyle(): object {
+    return {
+      backgroundImage: `linear-gradient(rgba(2, 0, 40, 0.85), rgba(2, 0, 40, 0.85)), url(${this.currentImage})`
+    };
+  }
 }
 </script>
-<style scoped lang="scss">
+<style lang="scss" scoped>
 #default {
+  min-height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-repeat: no-repeat;
 
+  #page-contents {
+    box-sizing: border-box;
+    height: 100%;
+  }
+}
+
+@media all and (max-width: 959px) {
+  #default {
+    #page-contents {
+      margin : 0 32px;
+    }
+  }
+}
+
+@media all and (min-width: 960px) {
+  #default {
+    #page-contents {
+      width: calc(100% - (208px * 2));
+      margin: 0 208px;
+    }
+  }
 }
 </style>
