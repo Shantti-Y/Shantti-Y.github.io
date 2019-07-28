86f9631873c90e71a505016032d94aeedea0bfd9
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-07-28 22:35:44 +0900

    fix entry data structure for skillSet story

--- storybook/stories/skillSet.stories.ts
+++ storybook/stories/skillSet.stories.ts
@@ -7,8 +7,11 @@ storiesOf('organisms/SkillSet', module)
     components: { SkillSet },
     data() {
       return {
-        categoryName: 'JavaScript',
-        categoryIcon: 'logo-javascript',
+        category: {
+          name: 'JavaScript',
+          icon: 'logo-javascript',
+          color: 'rgba(67, 239, 107, 1)'
+        },
         skills: [
           { name: 'JavaScript', percentage: 80 },
           { name: 'Vue.js', percentage: 70 },
@@ -16,88 +19,65 @@ storiesOf('organisms/SkillSet', module)
           { name: 'Webpack', percentage: 60 },
           { name: 'TypeScript', percentage: 60 }
         ],
-        color: 'rgba(67, 239, 107, 1)'
       }
     },
-    template: `
-      <skill-set
-        :categoryName="categoryName"
-        :categoryIcon="categoryIcon"
-        :skills="skills"
-        :color="color"
-      />
-    `
+    template: `<skill-set :category="category" :skills="skills" />`
   }))
   .add('red', () => ({
     components: { SkillSet },
     data() {
       return {
-        categoryName: 'BackEnd',
-        categoryIcon: 'md-cube',
+        category: {
+          name: 'BackEnd',
+          icon: 'md-cube',
+          color: 'rgba(201, 53, 53, 1)'
+        },
         skills: [
           { name: 'Ruby', percentage: 80 },
           { name: 'Kotlin', percentage: 60 },
           { name: 'Rails', percentage: 80 },
           { name: 'SpringBoot', percentage: 60 },
           { name: 'Postgresql', percentage: 70 }
-        ],
-        color: 'rgba(201, 53, 53, 1)'
+        ]
       }
     },
-    template: `
-      <skill-set
-        :categoryName="categoryName"
-        :categoryIcon="categoryIcon"
-        :skills="skills"
-        :color="color"
-      />
-    `
+    template: `<skill-set :category="category" :skills="skills" />`
   }))
   .add('orange', () => ({
     components: { SkillSet },
     data() {
       return {
-        categoryName: 'Markup',
-        categoryIcon: 'md-tv',
+        category: {
+          name: 'Markup',
+          icon: 'md-tv',
+          color: 'rgba(241, 133, 51, 1)'
+        },
         skills: [
           { name: 'HTML5', percentage: 80 },
           { name: 'CSS3', percentage: 80 },
           { name: 'Sass/Scss', percentage: 70 }
-        ],
-        color: 'rgba(241, 133, 51, 1)'
+        ]
       }
     },
-    template: `
-      <skill-set
-        :categoryName="categoryName"
-        :categoryIcon="categoryIcon"
-        :skills="skills"
-        :color="color"
-      />
-    `
+    template: `<skill-set :category="category" :skills="skills" />`
   }))
   .add('purple', () => ({
     components: { SkillSet },
     data() {
       return {
-        categoryName: 'Others',
-        categoryIcon: 'md-list',
+        category: {
+          name: 'Others',
+          icon: 'md-list',
+          color: 'rgba(71, 66, 228, 1)'
+        },
         skills: [
           { name: 'Git', percentage: 80 },
           { name: 'Github', percentage: 80 },
           { name: 'Gitlab', percentage: 70 },
           { name: 'Docker', percentage: 70 }
         ],
-        color: 'rgba(71, 66, 228, 1)'
       }
     },
-    template: `
-      <skill-set
-        :categoryName="categoryName"
-        :categoryIcon="categoryIcon"
-        :skills="skills"
-        :color="color"
-      />
-    `
+    template: `<skill-set :category="category" :skills="skills" />`
   }))
   ;
