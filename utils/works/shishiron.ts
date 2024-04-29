7a94ef1e52012905886e541cfad012051df87f6b
Takahiro Yoshioka <tsolvalouy@gmail.com>
2024-04-29 19:34:40 +0900

    画像をモバイル用に差し替え

--- utils/works/shishiron.ts
+++ utils/works/shishiron.ts
@@ -1,13 +1,11 @@
 import { Work } from '@/utils/works';
 
 const work: Work = {
-  headline: '',
+  headline: 'AI獅白ぼたんbot',
   role: '個人開発',
   screenShots: [
     { src: require('@/assets/images/works/shishiron/desktop1.jpg'), alt: 'shishiron-1' },
-    { src: require('@/assets/images/works/shishiron/desktop2.jpg'), alt: 'shishiron-2' },
-    { src: require('@/assets/images/works/shishiron/mobile1.jpg'), alt: 'shishiron-3' },
-    { src: require('@/assets/images/works/shishiron/mobile2.jpg'), alt: 'shishiron-4' }
+    { src: require('@/assets/images/works/shishiron/desktop2.jpg'), alt: 'shishiron-2' }
   ],
   overview: "コミックマーケット103で頒布した2次創作作品「獅白ぼたん」のなりきりLINE bot",
   background: "過去にコミックマーケットへのサークル参加/同人誌頒布の経験はあったものの頒布数に対して感想がもらえなかった為、感想を送信するハードルを下げて多く獲得するために開発。結果過去2件ほどしか得られていなかった感想メッセージが30件まで増加した",
